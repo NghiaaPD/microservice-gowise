b3f137dfcf9d7fdbe2341b20c2e9dd7d1a1cc8ff
NghiaaPD <123baconsau6969@gmail.com>
2025-06-10 07:37:02 +0000

    operation:fix java format

--- discovery-server/src/main/java/com/example/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/example/discovery_server/DiscoveryServerApplication.java
@@ -4,11 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main application class for discovery-server.
+ */
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
+public final class DiscoveryServerApplication {
 
-    public static void main(String[] args) {
+    private DiscoveryServerApplication() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Main method to start the Spring Boot application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 
